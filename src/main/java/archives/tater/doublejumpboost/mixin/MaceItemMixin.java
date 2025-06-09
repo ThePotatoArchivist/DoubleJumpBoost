ab3873ffdc727054aba22cbbda29e8d09758a9f4
JosiahFu <josiahfu@gmail.com>
2025-06-08 23:33:52 -0500

    Update to 1.21.5

--- src/main/java/archives/tater/doublejumpboost/mixin/MaceItemMixin.java
+++ src/main/java/archives/tater/doublejumpboost/mixin/MaceItemMixin.java
@@ -9,7 +9,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MaceItem.class)
 public class MaceItemMixin {
@@ -17,7 +17,7 @@ public class MaceItemMixin {
             method = "postHit",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;setIgnoreFallDamageFromCurrentExplosion(Z)V")
     )
-    private void recoverJump(ItemStack stack, LivingEntity target, LivingEntity attacker, CallbackInfoReturnable<Boolean> cir) {
+    private void recoverJump(ItemStack stack, LivingEntity target, LivingEntity attacker, CallbackInfo ci) {
         if (attacker instanceof ServerPlayerEntity serverPlayerEntity)
             ServerPlayNetworking.send(serverPlayerEntity, new RecoverJumpPayload(1));
     }
