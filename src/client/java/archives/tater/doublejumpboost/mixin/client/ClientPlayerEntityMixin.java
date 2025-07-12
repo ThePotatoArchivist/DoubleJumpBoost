b613ffb3210269cf03776ea388ede6d3445ddae0
JosiahFu <josiahfu@gmail.com>
2025-07-12 13:35:13 -0700

    Update to 1.21.7

--- src/client/java/archives/tater/doublejumpboost/mixin/client/ClientPlayerEntityMixin.java
+++ src/client/java/archives/tater/doublejumpboost/mixin/client/ClientPlayerEntityMixin.java
@@ -4,15 +4,14 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin extends PlayerEntity {
-	public ClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
-		super(world, pos, yaw, gameProfile);
+	public ClientPlayerEntityMixin(World world, GameProfile profile) {
+		super(world, profile);
 	}
 
 	@ModifyExpressionValue(
