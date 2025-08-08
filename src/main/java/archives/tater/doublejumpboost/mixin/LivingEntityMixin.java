af14c0da38303465a1976116db1a44aa5318e21b
JosiahFu <josiahfu@gmail.com>
2025-08-07 20:15:24 -0700

    Fix mixin causing weird knockback

--- src/main/java/archives/tater/doublejumpboost/mixin/LivingEntityMixin.java
+++ src/main/java/archives/tater/doublejumpboost/mixin/LivingEntityMixin.java
@@ -8,12 +8,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,8 +18,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 @Debug(export = true)
 public abstract class LivingEntityMixin extends Entity {
-    @Shadow public abstract void playSound(@Nullable SoundEvent sound);
-
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
@@ -42,7 +37,7 @@ public abstract class LivingEntityMixin extends Entity {
             at = @At(value = "INVOKE:LAST", target = "Lnet/minecraft/entity/LivingEntity;isOnGround()Z")
     )
     private boolean allowJump(boolean original, @Share("doubleJumped") LocalBooleanRef doubleJumped) {
-        if (original || !((Object) this instanceof PlayerEntity player)) return true;
+        if (original || !((Object) this instanceof PlayerEntity player)) return original;
         if (!DoubleJumpBoost.canJump(player)) return false;
         doubleJumped.set(true);
         return true;
